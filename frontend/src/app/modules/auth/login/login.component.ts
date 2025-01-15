100c9ea0f1bea40e54dc927c22bd6fba5b0add7d
ALFREDO-TUESTA-ABISRROR <alfred_2602@outlook.com>
2025-01-15 00:23:24 -0500

    se reorganizo la interfaz de gestion estudiantes, se valido el login y se creo la interfaz de inicio con los 3 botones principales de gestion de estudiantes, aulas y docentes

--- frontend/src/app/modules/auth/login/login.component.ts
+++ frontend/src/app/modules/auth/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    if (this.username === 'admin' && this.password === '123456') {
+      this.router.navigate(['/auth/home']); // Redirige al home
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  }
 }
